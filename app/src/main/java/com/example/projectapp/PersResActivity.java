60d4437ed98e1853142dc78b2686045e2e5f7c76
Ariel Shultz <106987539+ArielShultz@users.noreply.github.com>
2023-04-24 13:06:00 -0400

    Update PersResActivity.java

--- app/src/main/java/com/example/projectapp/PersResActivity.java
+++ app/src/main/java/com/example/projectapp/PersResActivity.java
@@ -26,6 +26,9 @@ public class PersResActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pers_res);
+        // part of old code that doesn't work anymore (delete next two lines)
+        roomName = getIntent().getStringExtra("Room Name"); // 1
+        time = getIntent().getStringExtra("Time"); // 2
 
         reserve = getIntent().getExtras().getStringArrayList("allReserve");
         resView(reserve);
@@ -51,4 +54,63 @@ public class PersResActivity extends AppCompatActivity {
         Intent intentMA = new Intent(this, DormSelection.class);
         startActivityForResult(intentMA, MAIN_ID);
     }
+
+// old code that doesnt work anymore
+public void onDeleteButtonClick(View v) {
+    deleteRes();
+    Intent i = new Intent(this, PageAfterDeleteRes.class); // need to make a new class
+    startActivityForResult(i, MAIN_ID);
+}
+
+private void deleteRes() {
+    String paramRoom = "Room Name=" + roomName;
+    String paramTime = "Time=" + time;
+    String urlLink = "http://10.0.2.2:3000/deleteRes?";
+    System.out.println(paramRoom);
+    System.out.println(paramTime);
+
+    try {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            try {
+                URL url = new URL(urlLink+paramRoom+paramTime);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                try {
+                    Scanner sc = null;
+                    conn.connect();
+
+                    // response comes back
+                    int responseCode = conn.getResponseCode();
+
+                    // make sure response status is 200 OK
+                    if (responseCode != 200) {
+                        System.out.println("Unexpected status code: " + responseCode);
+                    } else {
+                        // if we made it here, we got a good response to read
+                        sc = new Scanner(url.openStream());
+
+                        while (sc.hasNext()) {
+                            String line = sc.nextLine();
+                        }
+                    }
+                } catch (java.net.ConnectException ce) {
+                    ce.printStackTrace();
+                }
+            } catch (Exception a) {
+                a.printStackTrace();
+            }
+        });
+        executor = Executors.newSingleThreadExecutor();
+        try {
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException ie) {
+            throw new RuntimeException(ie);
+        }
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+
+}
 }
